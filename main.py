d97098364e6ed567d231c6653dfaf22218160ba0
Thom590 <twh590@gmail.com>
2024-11-22 12:31:16 -0700

    REV 6: MOVED FUNCTIONS INTO SEPERATE FILES FOR READABILITY

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import os
+from parse import parse
+from countWords import countWords
+#from createStrings import createStrings
 
-file_content = ""
-i = 0
-
-def parse(user_input):
-    global file_content
-    with open(user_input) as f:
-        file_content = f.read()
-
-def count(user_input):
-    global file_content, i
-    words = file_content.split()
-    i = 0
-    for each in words:
-        i += 1
 
 def main():
-    collected_input = os.path.expanduser(input("Define path to file:"))
-    parse(collected_input)
-    count(collected_input)
-    print(file_content)
-    print(i)
+
+    userInput = os.path.expanduser(input("Define path to file:"))
+
+    myParse = parse(userInput) 
+    print(myParse)
+
+    totalWords = (countWords(myParse))
+    print(totalWords)
+    #print(file_content)
+    #print(i)
     
 main()
