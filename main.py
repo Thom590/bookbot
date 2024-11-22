08817ac184d27e91f2c005a39eca13e90b6e31f0
Thom590 <twh590@gmail.com>
2024-11-22 09:06:17 -0700

    REV 2: First working state

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+import os
+
+file_content = ""
+
+def parse(user_input):
+    global file_content
+    with open(user_input) as f:
+        file_content = f.read()
+
+def main():
+    collected_input = input("Define path to file:")
+    collected_input = os.path.expanduser(collected_input)
+    parse(collected_input)
+    print("hello word")
+    print("collected input:", collected_input)
+    print("file_contnet:", file_content)
+    
+
+
+
+main()
