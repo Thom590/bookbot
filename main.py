6e1b3909d4d6bc907c9d81dbeee05d27f314de87
Thom590 <twh590@gmail.com>
2024-11-22 14:18:13 -0700

    REV 8: READY FOR REVIEW

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import os
 from parse import parse
 from countWords import countWords
-from createStrings import createWordsStrings
-
-
+from createStrings import createWordsList, createCharsString
+from countChars import countChars
 
 def main():
 
     userInput = os.path.expanduser('~/workspace/github.com/Thom590/bookbot/books/frankenstein.txt')
-
     myParse = parse(userInput) 
-    print(myParse)
 
-    wordsStrings = createWordsStrings(myParse)
-    totalWords = countWords(wordsStrings)
-    print(totalWords)
-    
+    wordsList = createWordsList(myParse)
+    charsString = createCharsString(myParse)
+
+    totalWords = countWords(wordsList)
+    totalChars = countChars(charsString)
+
+    #print(myParse)
+    #print(totalWords)
+    #print(totalChars)
+
+    print('----- Begin report of books/frankenstein.txt -----')
+    print(totalWords, "words found in the document")
+
+    for key, value in totalChars.items():
+        keyV = key
+        valV = value
+        print('The', keyV, 'charachter was found', valV, 'times')
 
     
 main()
